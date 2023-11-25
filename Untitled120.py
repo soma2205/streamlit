bface90b3d41ba88c241c0deae95c26a4fce5b4b
soma2205 <87004256+soma2205@users.noreply.github.com>
2023-11-25 18:28:41 +0530

    myapp

---
+++ Untitled120.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# app.py
+import streamlit as st
+
+def main():
+    st.title("My Streamlit App")
+    st.write("Hello, world!")
+
+if __name__ == "__main__":
+    main()
+
+
+# In[2]:
+
+
+streamlit run C:\Users\Somanath\anaconda3\lib\site-packages\ipykernel_launcher.py [ARGUMENTS]
+
+
+# In[ ]:
+
+
+
+
